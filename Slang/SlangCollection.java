6a6b36c1342b6b34f0b6c433d5f08c456793e3e5
Shenronis <hoangthien161201@gmail.com>
2021-12-19 12:28:44 +0700

    Add SlangCollection class
    SlangCollection:
    - Slang Collection as a collection for slang structure
    - Slang structure is made of HashMap<String K, ArrayList<String> V> with K as slang word, and V as its definitions

---
+++ Slang/SlangCollection.java
@@ -0,0 +1,198 @@
+package Slang;
+
+import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+/**
+ * Slang
+ * Created by Hoàng Thiện
+ * Date 12/19/2021 - 10:08 AM
+ * Description: This will be the structure to represent slang and its definition
+ */
+
+public class SlangCollection {
+    private HashMap<String, ArrayList<String>> collection;
+
+    public SlangCollection() {
+        collection = new HashMap<String, ArrayList<String>>();
+    }
+
+    public SlangCollection(SlangCollection slangCollection) {
+        this.collection = null;
+        if (slangCollection.collection != null) {
+            this.collection = new HashMap<>(slangCollection.collection);
+        }
+    }
+
+    public void addSlang(String slang, ArrayList<String> definition) throws IllegalAccessException, IllegalArgumentException {
+        if (!slang.isEmpty() && !definition.isEmpty()) {
+            if (!collection.containsKey(slang)) {
+                collection.put(slang, definition);
+            } else {
+                throw new IllegalAccessException();
+            }
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public void removeSlang(String slang) throws IllegalAccessException, IllegalArgumentException {
+        if (!slang.isEmpty()) {
+            if (collection.containsKey(slang)) {
+                collection.remove(slang);
+            } else {
+                throw new IllegalAccessException();
+            }
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public ArrayList<String> getSlang(String slang) {
+        if (collection.containsKey(slang)) {
+            return collection.get(slang);
+        }
+        return null;
+    }
+
+    public boolean doesSlangExist(String slang) {
+        return collection.containsKey(slang);
+    }
+
+    public void addDefinition(String slang, String definition) throws IllegalAccessException, IllegalArgumentException {
+        if (!slang.isEmpty() && !definition.isEmpty()) {
+            if (collection.containsKey(slang)) {
+                if (!collection.get(slang).contains(definition)) {
+                    collection.get(slang).add(definition);
+                } else {
+                    throw new IllegalAccessException();
+                }
+            } else {
+                ArrayList<String> definitions = new ArrayList<>();
+                definitions.add(definition);
+                addSlang(slang, definitions);
+            }
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public void addDefinition(String slang, ArrayList<String> defs) throws IllegalArgumentException {
+        for (String def : defs) {
+            try {
+                addDefinition(slang, def);
+            } catch (IllegalAccessException e) {
+                System.out.println("Definition duplication ["+slang+"]: " + def + "");
+            }
+        }
+    }
+
+    public HashMap<String, ArrayList<String>> getCollection() {
+        return collection;
+    }
+
+    private String FileBrowser(boolean isExporting) throws NullPointerException {
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setCurrentDirectory(new java.io.File("."));
+        FileNameExtensionFilter textFilter = new FileNameExtensionFilter("Text files (*.txt)", "txt");
+        fileChooser.setFileFilter(textFilter);
+
+        int currentDialog;
+
+        if (!isExporting) {
+            fileChooser.setDialogTitle("Select Student File (*.csv)");
+            currentDialog = fileChooser.showOpenDialog(null);
+        } else {
+            fileChooser.setDialogTitle("Export Student List (*.csv)");
+            currentDialog = fileChooser.showSaveDialog(null);
+        }
+
+        if (currentDialog == JFileChooser.APPROVE_OPTION) {
+            File file = fileChooser.getSelectedFile();
+            try {
+                if (file.exists()) {
+                    String path = file.getAbsolutePath();
+
+                    int result = JOptionPane.CLOSED_OPTION;
+                    if (fileChooser.getDialogType() == JFileChooser.OPEN_DIALOG) {
+                        result = JOptionPane.showConfirmDialog(fileChooser, "This will overwrite the current list, please be sure to export the current list!", "Overwrite?", JOptionPane.OK_CANCEL_OPTION);
+                        switch (result) {
+                            case JOptionPane.OK_OPTION:
+                                return path;
+                            case JOptionPane.CLOSED_OPTION, JOptionPane.CANCEL_OPTION:
+                                return "";
+                        }
+                    } else if (fileChooser.getDialogType() == JFileChooser.SAVE_DIALOG) {
+                        result = JOptionPane.showConfirmDialog(fileChooser, "This will overwrite the current file. Continue?!", "Overwrite?", JOptionPane.OK_CANCEL_OPTION);
+                    }
+
+                    switch (result) {
+                        case JOptionPane.OK_OPTION:
+                            return path;
+                        case JOptionPane.CLOSED_OPTION, JOptionPane.CANCEL_OPTION:
+                            return "";
+                    }
+                }
+                else if (isExporting) {
+                    String path = file.getAbsolutePath();
+                    if(!path.endsWith(".txt") ) {
+                        path += ".txt";
+                    }
+                    return path;
+                }
+            } catch (NullPointerException | SecurityException e) {
+                e.printStackTrace();
+            }
+        }
+        return "";
+    }
+
+    public void ImportSlangDictionary() throws IOException, IllegalArgumentException{
+        String path = FileBrowser(false);
+        if (!path.isEmpty()) {
+            int count = 0;
+            String row;
+            collection.clear();
+            String lastSlang = "";
+            try {
+                BufferedReader reader = new BufferedReader(new FileReader(path));
+                while((row = reader.readLine()) != null) {
+                    String[] data = row.split("`");
+                    if (data.length > 1) {
+                        lastSlang = data[0];
+                        String[] defs = data[1].split("\\|");
+                        ArrayList<String> newListOfDefs = new ArrayList<>(Arrays.asList(defs));
+                        try {
+                            addSlang(lastSlang, newListOfDefs);
+                        } catch (IllegalAccessException e) {
+                            System.out.println("Slang Duplication ["+lastSlang+"] at line " + count);
+                            System.out.println("Proceed to add any new definition...");
+                            addDefinition(lastSlang, newListOfDefs);
+                            continue;
+                        }
+                    } else {
+                        if (!lastSlang.equals("")) {
+                            addDefinition(lastSlang, data[0]);
+                        } else {
+                            throw new IllegalArgumentException();
+                        }
+                    }
+                    count++;
+                }
+                reader.close();
+
+                System.out.println("Successfully imported " + path);
+                System.out.println(count + " Slang(s) was imported!");
+            } catch (IllegalArgumentException e) {
+                e.printStackTrace();
+                System.out.println("Error loading data at line " + count);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
